f606aec37c165f3989fc577916553d6a576b768e
dotnetdev50 <145797379+dotnetdev50@users.noreply.github.com>
2026-03-02 14:05:30 +0530

    Change router history mode to hash

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,7 +1,7 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Dashboard from "@/pages/Dashboard.vue";
 
 export default createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [{ path: "/", component: Dashboard }],
 });
